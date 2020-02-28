1c88c836abe37a9a426997db2bd150efe3141c38
Ilya Erlingas <Aligruit@gmail.com>
2020-02-28 17:23:19 +0300

    +OOP

--- Contest.py
+++ Contest.py
@@ -1,37 +1,49 @@
 import datetime
 import time
+
 start_time = time.monotonic()
 user_input = input("Введите комманду и числа оперции: ")
 """Ввод пользователем поочередно: сначала функция => потом два числа"""
 input_list = user_input.split(" ")
 """Разбитие ввода на части через пробел"""
 
-try:
-    """Проверка условий ввода и приравнивание к integers чисел"""
-    mark = (input_list[0])
-    a = int(input_list[1])
-    b = int(input_list[2])
-    if mark == "+":
-        result = a + b
-    elif mark == "/":
-        result = a / b
-    elif mark == "-":
-        result = a - b
-    elif mark == "*":
-        result = a * b
-    print(result)
-    """Проверка на шибки ввода - /0, ввод букв вместо чисел, недостаточное количество чисел"""
-except ZeroDivisionError as Z:
-    print(Z, type(Z))
-    print("Делить на ноль нельзя!")
-except ValueError as V:
-    print(V, type(V))
-    print("Неверный ввод данных")
-except IndexError as I:
-    print(I, type(I))
-    print("Маловато операндов")
-finally:
-    print("Этот блок будет выполнятся всегда")
+
+class PolandNotation:
+    def __init__(self, input_list_):
+        self.input_list = input_list_
+
+    def notation(self):
+        try:
+            """Проверка условий ввода и приравнивание к integers чисел"""
+            result = None
+            mark = (self.input_list[0])
+            a = int(self.input_list[1])
+            b = int(self.input_list[2])
+            if mark == "+":
+                result = a + b
+            elif mark == "/":
+                result = a / b
+            elif mark == "-":
+                result = a - b
+            elif mark == "*":
+                result = a * b
+            print(result)
+            """Проверка на шибки ввода - /0, ввод букв вместо чисел, недостаточное количество чисел"""
+        except ZeroDivisionError as Z:
+            print(Z, type(Z))
+            print("Делить на ноль нельзя!")
+        except ValueError as V:
+            print(V, type(V))
+            print("Неверный ввод данных")
+        except IndexError as Index:
+            print(Index, type(Index))
+            print("Маловато операндов")
+        finally:
+            print("Этот блок будет выполнятся всегда")
+
+
+notation = PolandNotation(input_list)
+notation.notation()
 
 
 class loger:
@@ -58,6 +70,6 @@ class Except:
 
 if __name__ == 'main':
     log = loger('log.txt')
-    with loger('log.txt') as log:
+    with loger('log.txt') as log_new:
         log.write_in_log()
         print(log)
